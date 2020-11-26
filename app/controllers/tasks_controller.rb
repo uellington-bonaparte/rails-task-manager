8b556d66e0b97c6114925206655d01b6e39c90d2
Uellington Cortes <code.uellington@gmail.com>
2020-11-26 14:27:42 -0300

    index and show tasks implemented

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,14 @@
 class TasksController < ApplicationController
+  # As a user, I can list tasks
+  def index
+    @tasks = Task.all
+  end
+
+  # As a user, I can view the details of a task
+  def show
+    @task = Task.find(params[:id])
+  end
+  # As a user, I can add a new task
+  # As a user, I can edit a task (mark as completed / update title & details)
+  # As a user, I can remove a task
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # As a user, I can list tasks
+  get 'tasks', to: 'tasks#index'
+
+  # As a user, I can view the details of a task
+  get 'tasks/:id', to: 'tasks#show'
+
+  # As a user, I can add a new task
+  # As a user, I can edit a task (mark as completed / update title & details)
+  # As a user, I can remove a task
 end
